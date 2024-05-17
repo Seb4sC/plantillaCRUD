6267bec1bba83c7c84a0348f76e997190da55125
Seb4sC <sacastro081@soy.sena.edu.co>
2024-05-17 12:19:46 -0500

    correciones

--- resources/views/profesors/create.blade.php
+++ resources/views/profesors/create.blade.php
@@ -50,9 +50,15 @@
                 <input name="departamento" type="text" class="form-control" id="departamento" placeholder="departamento">
                 <label for="departamento">Departamento</label>
             </div>
+
+            {{-- <input type="file" name="urlPdf" class="form-control-file" accept="pdf/*"> --}}
             <button type="submit" class="btn btn-success w-100">Crear</button>
         </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
+
+{{-- @foreach ($users as $user)
+<option value="{{$user->id}}">{{$user->name}}</option>
+@endforeach --}}
